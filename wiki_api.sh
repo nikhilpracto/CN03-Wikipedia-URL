7b14894831ebb94aae13fadcf03365c7e117edf4
nikhilpracto <nikhil.nagrale@practo.com>
2022-03-31 10:58:06 +0530

    wikipedia link adder

---
+++ wiki_api.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "What do you wanna know about?"
+read searchData
+searchData="${searchData// /_}"
+echo "Searching for the link ...."
+wikipedia="https://en.wikipedia.org/wiki/$value"
+echo "${wikipedia}">> "wikipedia.txt"
+echo "Appending the link to file ...."
+echo "Action Completed"
